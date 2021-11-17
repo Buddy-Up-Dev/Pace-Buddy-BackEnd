3010bd63a8d854d56d57f0829de1dfcd5a5fc1c3
Song-YunMin <thd930308@naver.com>
2021-11-17 16:46:19 +0900

    feat: auth 테스트 추가

--- src/auth/auth.resolver.ts
+++ src/auth/auth.resolver.ts
@@ -1,4 +1,4 @@
-import { Query, Resolver } from "@nestjs/graphql";
+import { Args, Context, Mutation, Query, Resolver } from "@nestjs/graphql";
 import { AuthService } from "./auth.service";
 
 @Resolver()
@@ -6,9 +6,16 @@ export class AuthResolver {
   constructor(private authService: AuthService) {
   }
 
-  @Query('userNickname')
-  async userNickname(context: object): Promise<string> {
-    return this.authService
+  @Query('testToken')
+  async testToken(context: object): Promise<boolean> {
+    await this.authService.tokenTest();
+    return true;
+  }
+
+  @Mutation('naverLogin')
+  async naverLogin(context: object, @Args('accessToken') accessToken: string): Promise<string> {
+
+    return 'a'
   }
 
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,9 +1,23 @@
 import { Injectable } from '@nestjs/common';
+import { JwtService } from "@nestjs/jwt";
+import { InjectRepository } from "@nestjs/typeorm";
+import { UserRepository } from "../../dist/user/user.repository";
 
 @Injectable()
 export class AuthService {
-  constructor() {}
+  constructor(
+    @InjectRepository(UserRepository)
+    private userRepository: UserRepository,
+    private jwtService: JwtService
+  ) {
+  }
 
+  public async tokenTest(): Promise<string> {
+    const payload: string = '1';
+    const jwtToken = await this.jwtService.sign(payload);
+    console.info(jwtToken);
 
+    return 'a';
+  }
 
 }
