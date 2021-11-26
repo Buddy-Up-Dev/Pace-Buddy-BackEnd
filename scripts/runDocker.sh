1f544ece7487b7c760d4076ff2efb36b0677ac36
Song-YunMin <thd930308@naver.com>
2021-11-27 05:49:27 +0900

    test: Modify Docker sh

--- scripts/runDocker.sh
+++ scripts/runDocker.sh
@@ -1,20 +1,14 @@
 
 if [ "$DEPLOYMENT_GROUP_NAME" == "dev" ]
 then
-  # Remove any anonymous volumes attached to containers
   docker-compose -f /deploy/docker-compose.yml rm -v
-  # build images and run containers
   docker-compose -f /deploy/docker-compose.yml up --detach --renew-anon-volumes
-  elif [ "$DEPLOYMENT_GROUP_NAME" == "stage" ]
+elif [ "$DEPLOYMENT_GROUP_NAME" == "stage" ]
 then
-  # Remove any anonymous volumes attached to containers
   docker-compose -f /deploy/docker-compose.yml rm -v
-  # build images and run containers
   docker-compose -f /deploy/docker-compose.yml up --detach --renew-anon-volumes
 elif [ "$DEPLOYMENT_GROUP_NAME" == "production" ]
 then
-  # Remove any anonymous volumes attached to containers
   docker-compose -f /deploy/docker-compose.yml rm -v
-  # build images and run containers
   docker-compose -f /deploy/docker-compose.yml up --detach --renew-anon-volumes
 fi
