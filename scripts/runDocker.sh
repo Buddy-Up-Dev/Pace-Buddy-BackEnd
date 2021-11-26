57ed37c1208db41558a6d7f727a81a9464344f01
Song-YunMin <thd930308@naver.com>
2021-11-27 05:36:30 +0900

    test: Docker 배포 정책 변경

--- scripts/runDocker.sh
+++ scripts/runDocker.sh
@@ -5,4 +5,16 @@ then
   docker-compose -f /deploy/docker-compose.yml rm -v
   # build images and run containers
   docker-compose -f /deploy/docker-compose.yml up --detach --renew-anon-volumes
+  elif [ "$DEPLOYMENT_GROUP_NAME" == "stage" ]
+then
+  # Remove any anonymous volumes attached to containers
+  docker-compose -f /deploy/docker-compose.yml rm -v
+  # build images and run containers
+  docker-compose -f /deploy/docker-compose.yml up --detach --renew-anon-volumes
+elif [ "$DEPLOYMENT_GROUP_NAME" == "production" ]
+then
+  # Remove any anonymous volumes attached to containers
+  docker-compose -f /deploy/docker-compose.yml rm -v
+  # build images and run containers
+  docker-compose -f /deploy/docker-compose.yml up --detach --renew-anon-volumes
 fi
