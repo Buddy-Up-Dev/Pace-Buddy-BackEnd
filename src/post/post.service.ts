0aa87f49795b7eb4c0f64846ef851b9b39aa8d04
Song-YunMin <thd930308@naver.com>
2021-12-10 02:26:32 +0900

    feat: 날짜 형식, 알고리즘 수정

--- src/post/post.service.ts
+++ src/post/post.service.ts
@@ -19,11 +19,11 @@ export class PostService {
     : Promise<{ likeArray: number[]; PostData: PostInformation[] }> {
 
     let userIndex = -1;
-    const req = context['req']['headers']['authorization'];
+    const req = context["req"]["headers"]["authorization"];
     if (req !== undefined) {
       const token = req.substr(7, req.length - 7);
-      const decode = await authService['decodeToken'](token);
-      userIndex = decode['userIndex'];
+      const decode = await authService["decodeToken"](token);
+      userIndex = decode["userIndex"];
       console.info(userIndex);
     }
 
@@ -43,12 +43,12 @@ export class PostService {
     : Promise<{ likeArray: number[]; PostData: PostInformation[] }> {
 
     let userIndex = -1;
-    const req = context['req']['headers']['authorization'];
+    const req = context["req"]["headers"]["authorization"];
     console.info(req);
     if (req !== undefined) {
       const token = req.substr(7, req.length - 7);
-      const decode = await authService['decodeToken'](token);
-      userIndex = decode['userIndex'];
+      const decode = await authService["decodeToken"](token);
+      userIndex = decode["userIndex"];
       console.info(userIndex);
     }
 
@@ -68,9 +68,9 @@ export class PostService {
   public async getMyPost(context: object, userService: object, likeService: object, authService: object)
     : Promise<{ likeArray: number[]; PostData: PostInformation[] }> {
 
-    const req = context['req']['headers']['authorization'];
+    const req = context["req"]["headers"]["authorization"];
     const token = req.substr(7, req.length - 7);
-    const decode = await authService['decodeToken'](token);
+    const decode = await authService["decodeToken"](token);
     const userIndex = decode["userIndex"];
 
     try {
@@ -87,8 +87,8 @@ export class PostService {
     for (const node of data) {
       returnData.push({
         Post: node,
-        User: await userService['getUser'](node.userIndex),
-        Like: await likeService['getLikeByPost'](node.postIndex)
+        User: await userService["getUser"](node.userIndex),
+        Like: await likeService["getLikeByPost"](node.postIndex)
       });
       node.uploadDate = JSON.stringify(node.uploadDate).slice(6, 8) + "." + JSON.stringify(node.uploadDate).slice(9, 11);
     }
@@ -97,7 +97,7 @@ export class PostService {
     return getPostData.getPostData();
   }
 
-  private sortByPopularity(data): PostDataDto['PostData'] {
+  private sortByPopularity(data): PostDataDto["PostData"] {
     console.info(data);
     return data.sort((a, b) => {
       return parseFloat(b.Like) - parseFloat(a.Like);
@@ -106,10 +106,10 @@ export class PostService {
 
   public async addPost(context: object, uploadDate: string, exercise: number, content: string,
                        condition: number, feedOpen: number, authService: object): Promise<boolean> {
-    const req = context['req']['headers']['authorization'];
+    const req = context["req"]["headers"]["authorization"];
     const token = req.substr(7, req.length - 7);
-    const decode = await authService['decodeToken'](token);
-    const userIndex = decode['userIndex'];
+    const decode = await authService["decodeToken"](token);
+    const userIndex = decode["userIndex"];
     try {
       await this.postRepository.save(new Post(userIndex, uploadDate, exercise, content, condition, feedOpen).getPostInfo());
       return true;
@@ -120,16 +120,16 @@ export class PostService {
 
   public async likePost(context: object, postIndex: number, isDelete: boolean,
                         likeService: object, authService: object): Promise<boolean> {
-    const req = context['req']['headers']['authorization'];
+    const req = context["req"]["headers"]["authorization"];
     const token = req.substr(7, req.length - 7);
-    const decode = await authService['decodeToken'](token);
-    const userIndex = decode['userIndex'];
+    const decode = await authService["decodeToken"](token);
+    const userIndex = decode["userIndex"];
     try {
       if (isDelete) {
-        await likeService['deleteLike'](postIndex, userIndex);
+        await likeService["deleteLike"](postIndex, userIndex);
         return true;
       } else {
-        await likeService['addLike'](postIndex, userIndex);
+        await likeService["addLike"](postIndex, userIndex);
         return true;
       }
     } catch (e) {
@@ -138,33 +138,33 @@ export class PostService {
   }
 
   public async getLikeCount(userIndex: number, likeService: object): Promise<number[]> {
-    const likeArray: Like[] = await likeService['getLikeByUser'](userIndex);
+    const likeArray: Like[] = await likeService["getLikeByUser"](userIndex);
     return likeArray.map(node => node.postIndex);
   }
 
   // TODO : report 기능 구체화
   public async reporting(context: object, authService: object, reportService: object, exerciseService: object): Promise<number> {
-    const req = context['req']['headers']['authorization'];
+    const req = context["req"]["headers"]["authorization"];
     const token: string = req.substr(7, req.length - 7);
-    const decode: object = await authService['decodeToken'](token);
-    const userIndex: number = decode['userIndex'];
+    const decode: object = await authService["decodeToken"](token);
+    const userIndex: number = decode["userIndex"];
 
     // 유저의 최근 5개 기록 조회
     const posts: Post[] = await this.postRepository.find({
-      select: ['condition', 'exercise', 'uploadDate'],
+      select: ["condition", "exercise", "uploadDate"],
       where: { userIndex: userIndex },
-      order: { uploadDate: 'DESC' },
+      order: { uploadDate: "DESC" },
       take: 10
     });
 
     const returnData: object = await this.getReportData(posts);
-    const condition = await reportService['getReportData'](returnData['condition']);
-    const exercise = await exerciseService['getExerciseName'](returnData['exerciseIndex']);
-    const date = await this.getDateData(returnData['date']);
+    const condition = await reportService["getReportData"](returnData["condition"]);
+    const exercise = await exerciseService["getExerciseName"](returnData["exerciseIndex"]);
+    const date = await this.getDateData(returnData["date"]);
 
-    console.log('condition >', condition);
-    console.log('exercise >', exercise);
-    console.log('date >', date);
+    console.log("condition >", condition);
+    console.log("exercise >", exercise);
+    console.log("date >", date);
 
     return 1;
   }
@@ -187,31 +187,46 @@ export class PostService {
 
     const date = posts.map(node => node.uploadDate).reverse();
 
-    return { condition: condition, exerciseIndex: exerciseIndex, date: date }
+    return { condition: condition, exerciseIndex: exerciseIndex, date: date };
   }
 
   private getDateData(dateList: string[]): string {
     const newDateList: Date[] = this.stringToDate(dateList);
-    // TODO : newDateList의 요소 간 날짜 차이 구하기
-    console.log('newDateList >', newDateList);
-    console.log('날짜 차이 test >', newDateList[1].getDate() - newDateList[0].getDate());
-    return 'a';
+    // console.log('newDateList >', newDateList);
+    // console.log('날짜 차이 test >', newDateList[1].getDate() - newDateList[0].getDate());
+    // console.info(newDateList[1].getDate());
+
+    // const result = newDateList.reduce((acc, x, index, array) => {
+    //   console.info(array[index]);
+    //   array[index].getDate() - array[index-1].getDate()
+    //   return 5;
+    // })
+
+    // TODO: 앞 Node도 포함시켜야 함 (Array 보다 Counting 이 적절한 방법)
+    const result = newDateList.filter((node, index, array) => {
+      if (index > array.length - 2) return;
+      return Math.ceil((array[index + 1].getTime() - array[index].getTime()) / (1000*3600*24)) <= 2;
+    });
+
+
+    return "a";
   }
 
   private stringToDate(dateList: string[]): Date[] {
     return dateList.map(date =>
-        new Date(Number(date.split('.')[0]), Number(date.split('.')[1]) - 1, Number(date.split('.')[2]))
+      // TODO: Date + 1
+      new Date(Number(date.split(".")[0]), Number(date.split(".")[1]) - 1, Number(date.split(".")[2]) + 1)
     );
   }
 
   public async getMyDate(context: object, authService: object): Promise<string[]> {
-    const req = context['req']['headers']['authorization'];
+    const req = context["req"]["headers"]["authorization"];
     const token = req.substr(7, req.length - 7);
-    const decode = await authService['decodeToken'](token);
-    const userIndex = decode['userIndex'];
+    const decode = await authService["decodeToken"](token);
+    const userIndex = decode["userIndex"];
     try {
       return (await this.postRepository.find({
-        select: ['uploadDate'],
+        select: ["uploadDate"],
         where: { userIndex: userIndex }
       })).map(node => node.uploadDate);
     } catch (e) {
@@ -221,10 +236,10 @@ export class PostService {
 
   public async modifyPost(context: object, postIndex: number, uploadDate: string,
                           exercise: number, content: string, condition: number, feedOpen: number, authService: object): Promise<boolean> {
-    const req = context['req']['headers']['authorization'];
+    const req = context["req"]["headers"]["authorization"];
     const token = req.substr(7, req.length - 7);
-    const decode = await authService['decodeToken'](token);
-    const userIndex = decode['userIndex'];
+    const decode = await authService["decodeToken"](token);
+    const userIndex = decode["userIndex"];
     try {
       await this.postRepository.save({
         postIndex: postIndex, uploadDate: uploadDate, exercise: exercise,
@@ -237,10 +252,10 @@ export class PostService {
   }
 
   public async deletePost(context: object, postIndex: number, authService: object): Promise<boolean> {
-    const req = context['req']['headers']['authorization'];
+    const req = context["req"]["headers"]["authorization"];
     const token = req.substr(7, req.length - 7);
-    const decode = await authService['decodeToken'](token);
-    const userIndex = decode['userIndex'];
+    const decode = await authService["decodeToken"](token);
+    const userIndex = decode["userIndex"];
     try {
       await this.postRepository.delete({ postIndex: postIndex });
       return true;
@@ -257,7 +272,6 @@ export class PostService {
       throw new Error(e);
     }
   }
-
 
 
 }
