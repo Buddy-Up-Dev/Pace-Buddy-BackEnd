92369313e1806cdd7c792a13ff56f66a55e25548
Serin-Yoon <bcode429@gmail.com>
2021-12-29 13:45:46 +0900

    refactor: 인덴트 통일

--- src/post/post.resolver.ts
+++ src/post/post.resolver.ts
@@ -38,7 +38,7 @@ export class PostResolver {
 
   @Query('reporting')
   async reporting(@Context() context: object):
-      Promise<ReportData> {
+    Promise<ReportData> {
     return await this.postService.reporting(context, this.authService, this.reportService, this.exerciseService);
   }
 
@@ -49,13 +49,15 @@ export class PostResolver {
 
   @Mutation('addPost')
   async addPost(@Context() context: object, @Args('uploadDate') uploadDate: string, @Args('exercise') exercise: number,
-      @Args('content') content: string, @Args('condition') condition: number, @Args('feedOpen') feedOpen: number
+    @Args('content') content: string, @Args('condition') condition: number, @Args('feedOpen') feedOpen: number
   ): Promise<Boolean> {
     return await this.postService.addPost(context, uploadDate, exercise, content, condition, feedOpen, this.authService);
   }
 
   @Mutation('modifyPost')
-  async modifyPost(@Context() context: object, @Args('postIndex') postIndex: number, @Args('uploadDate') uploadDate: string, @Args('exercise') exercise: number, @Args('content') content: string, @Args('condition') condition: number, @Args('feedOpen') feedOpen: number
+  async modifyPost(@Context() context: object, @Args('postIndex') postIndex: number, @Args('uploadDate') uploadDate: string,
+    @Args('exercise') exercise: number, @Args('content') content: string, @Args('condition') condition: number,
+    @Args('feedOpen') feedOpen: number
   ): Promise<Boolean> {
     return await this.postService.modifyPost(context, postIndex, uploadDate, exercise, content, condition, feedOpen, this.authService);
   }
