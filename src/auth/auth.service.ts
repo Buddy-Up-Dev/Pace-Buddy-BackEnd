4379ab83490ccf2e446e48f56c2b93f7f5b0edb9
Song-YunMin <thd930308@naver.com>
2021-12-29 13:26:15 +0900

    refactor: 필요없는 메서드 tokenTest 제거

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -9,21 +9,13 @@ export class AuthService {
     private jwtService: JwtService
   ) {}
 
-  public async tokenTest(context: object): Promise<string> {
-    const payload: object = {userIndex: 1};
-    const jwtToken = this.jwtService.sign(payload);
-    return 'a';
-  }
-
   public async naverLogin(accessToken: string, userService: any): Promise<string> {
-    // userInfo(id) 조회
     const response = await fetch('https://openapi.naver.com/v1/nid/me', {
       method: 'GET',
       headers: { 'Authorization' : `Bearer ${accessToken}` }
     })
     const result = await response.text();
 
-    // 회원가입 및 로그인
     try {
       if (response.status === 200) {
         const userInfo: object = await userService.checkNewUser(JSON.parse(result).response.id, 'naver');
