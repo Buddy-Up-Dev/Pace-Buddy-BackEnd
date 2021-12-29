1fedcd450aaabdadee6b840d41914f9c36de0260
Serin-Yoon <bcode429@gmail.com>
2021-12-29 14:59:26 +0900

    refactor: 불필요한 import 제거

--- src/like/like.service.ts
+++ src/like/like.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@nestjs/common';
-import { getRepository, Repository } from "typeorm";
+import { Repository } from "typeorm";
 import { Like } from './like.entity';
 import { InjectRepository } from "@nestjs/typeorm";
 
